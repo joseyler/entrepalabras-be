82cd0f699baca51e5bc0d6c1ebf658d1e14b2c7e
Marlene Carli <maarlen.car@hotmail.com>
2024-05-18 15:53:56 -0300

    agrega endpoint de login con  generacion de token y validacion de pw con bcrypt. agrega ruta protegida en controller de usuarios

---
+++ src/common/services/db.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import {
+  FieldPacket,
+  Pool,
+  PoolConnection,
+  ResultSetHeader,
+  RowDataPacket,
+  createPool,
+} from 'mysql2/promise';
+
+@Injectable()
+export class DBService {
+  private pool: Pool;
+
+  constructor() {
+    this.pool = createPool({
+      port: 3306,
+      database: 'entrepalabras',
+      password: 'root',
+      host: 'localhost',
+      user: 'root',
+      connectionLimit: 10,
+    });
+  }
+
+  executeQuery = async (
+    sql: string,
+    param: any[],
+  ): Promise<ResultSetHeader> => {
+    const connection: PoolConnection = await this.pool.getConnection();
+    const [result]: [ResultSetHeader, FieldPacket[]] =
+      await connection.query<ResultSetHeader>(sql, param);
+    this.pool.releaseConnection(connection);
+    return result;
+  };
+
+  executeSelect = async (
+    sql: string,
+    param: any[],
+  ): Promise<RowDataPacket[]> => {
+    const connection: PoolConnection = await this.pool.getConnection();
+    const [result]: [RowDataPacket[], FieldPacket[]] = await connection.query<
+      RowDataPacket[]
+    >(sql, param);
+    this.pool.releaseConnection(connection);
+    return result;
+  };
+}
